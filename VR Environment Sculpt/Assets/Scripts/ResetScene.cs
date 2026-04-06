cc3b8682b014a58632ed14ed841be5c40c2c5583
Sloane Taylor <165952341+Slomeow@users.noreply.github.com>
2026-04-06 14:22:02 -0500

    Redid Locomotion for Amplify showcase and reset button for build

--- VR Environment Sculpt/Assets/Scripts/ResetScene.cs
+++ VR Environment Sculpt/Assets/Scripts/ResetScene.cs
@@ -1,14 +1,47 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+using System.Collections.Generic;
 
 public class ResetScene : MonoBehaviour
 {
+    private InputDevice rightHandDevice;
+    private bool wasButtonPressed = false;
+
+    void Start()
+    {
+        InitializeRightHandDevice();
+    }
+
+    void InitializeRightHandDevice()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, devices);
+
+        if (devices.Count > 0)
+        {
+            rightHandDevice = devices[0];
+        }
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Try to find the device if it hasn't been found yet
+        if (!rightHandDevice.isValid)
+        {
+            InitializeRightHandDevice();
+            return;
+        }
+
+        rightHandDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool isPressed);
+
+        // Only trigger on the initial press, not while held down
+        if (isPressed && !wasButtonPressed)
         {
             ReloadCurrentScene();
         }
+
+        wasButtonPressed = isPressed;
     }
 
     void ReloadCurrentScene()
